a80b9310794ec374b4bf09c11fed761c689e5697
Matheus Freitas <122810512+mtfreitas-dev@users.noreply.github.com>
2025-09-03 11:51:09 -0300

    feat(lwc): add feedbackDashboard component and update controller for filterDashboard data requests
    - Create feedbackDashboard component for user feedback display
    - Enhance controller with new methods to support filterDashboard data requests
    - Add server-side logic for filtered data retrieval

---
+++ force-app/main/default/lwc/feedbackDashboard/feedbackDashboard.js
@@ -0,0 +1,39 @@
+import { LightningElement } from 'lwc';
+import getDataGraficBar from '@salesforce/apex/controller.getDataGraficBar';
+import getDataGraficDonut from '@salesforce/apex/controller.getDataFraficDonut'
+
+export default class FeedbackDashboard extends LightningElement {
+
+    filtrar = false;
+    period;
+    category = [];
+    barGraficdata;
+    barDonutGrafic;
+
+    handleFiltrar(event){
+        if(this.filtrar == true){
+            this.filtrar = false;
+        }else{
+            this.filtrar = true;
+        }
+
+        this.period = event.detail.period;
+        this.category = event.detail.category;
+        this.loadData();
+    }
+
+    getData(event) {
+    this.period = event.detail.period;
+    this.category = event.detail.category;
+
+    // Mostrar valores
+    console.log('Categorias (spread): ', [...this.category]);
+    console.log('Categorias (JSON): ', JSON.stringify(this.category));
+
+    this.loadData();    
+}
+
+    loadData(){
+        console.log('testando...');     
+    }
+}
